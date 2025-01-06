01b740cb2a32fa5aa9d53dbafd1397e214a9b2ba
Ananddd06 <122982405+Ananddd06@users.noreply.github.com>
2025-01-06 21:59:56 +0530

    Checking

--- demo.py
+++ demo.py
@@ -8,10 +8,14 @@ logging.info("Addition of the 2 numbers ")
 try : 
     def add(a : int , b : int):
         return a + b
+    
+    def sub(a : int , b : int):
+        return a - b
 
     num1 = int(input("Enter the number"))
     num2 = int(input("Enter the number"))
     print("The addition of two number " , add(num1 , num2))
+    print("The subtraction of two number " , sub(num1 , num2))
 except Exception as e:
     raise Custom_Exception(e , sys)
 
