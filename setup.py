7417817a5634d1f73dc261f0955b16dfbbf2b3b0
Ananddd06 <122982405+Ananddd06@users.noreply.github.com>
2025-01-10 21:26:46 +0530

    Edited in the setup file

--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
-from setuptools import find_packages , setup 
+from setuptools import find_packages , setup
+import os 
+import sys
+from us_visa.exception import Custom_Exception 
+from us_visa.logger import logging
 from typing import List
 
 
 HYPEN_E_DOT = '-e .'
 def get_requirements(file_path : str)->List[str]:
     requirements = []
-    with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.replace("/n" , "") for req in requirements]
+    try:
+        with open(file_path) as file_obj:
+            requirements = file_obj.readlines()
+            requirements = [req.replace("\n" , "") for req in requirements]
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
+            if HYPEN_E_DOT in requirements:
+                requirements.remove(HYPEN_E_DOT)
         
-        return requirements
+        logging.info("The requirements is Done successfully")
+    except Exception as e:
+        raise Custom_Exception(e , sys)
+    
+    return requirements
 
 setup(
     name = "us_visa" ,
