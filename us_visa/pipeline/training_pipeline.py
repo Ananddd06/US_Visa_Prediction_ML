7babec0aa40819f6aec6a590d078336d0fd99f3e
Ananddd06 <122982405+Ananddd06@users.noreply.github.com>
2025-01-13 10:45:24 +0530

    Added trainig pipeline for the data validation

--- us_visa/pipeline/training_pipeline.py
+++ us_visa/pipeline/training_pipeline.py
@@ -2,12 +2,16 @@ import sys
 from us_visa.exception import Custom_Exception
 from us_visa.logger import logging
 from us_visa.components.data_ingestion import DataIngestion
-from us_visa.entity.config_entity import DataIngestionConfig
-from us_visa.entity.artifact_entity import DataIngestionArtifact
+from us_visa.components.data_validation import DataValidation
+from us_visa.entity.config_entity import (DataIngestionConfig,
+                                          DataValidationConfig)
+from us_visa.entity.artifact_entity import (DataIngestionArtifact , 
+                                            DataValidationArtifact)
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config = DataValidationConfig()
     
     def start_data_ingestion(self)->DataIngestionArtifact:
         try:
@@ -21,8 +25,23 @@ class TrainPipeline:
         except Exception as e:
             raise Custom_Exception(e , sys)
     
+    def start_data_validation(self)->DataValidationArtifact:
+        logging.info(f"Entered the start_data_validation of the TrainPipeline Class")
+        try:
+            data_validation = DataValidation(data_ingestion_artifact = data_ingestion_artifact , 
+                                             data_validation_config = self.data_validation_config)
+            data_validation_artifact = data_validation.initiate_data_validation()
+            logging.info("Performed the data validation operation")
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+            return data_validation_artifact
+        except Exception as e:
+            raise Custom_Exception(e, sys)
+    
     def run_pipeline(self, )-> None:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact = data_ingestion_artifact)
         except Exception as e:
             raise Custom_Exception(sys,e)
