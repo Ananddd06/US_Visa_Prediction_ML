8dd423960e663315c356c6332e42417d7930803b
Ananddd06 <122982405+Ananddd06@users.noreply.github.com>
2025-01-14 09:29:12 +0530

    Added the constants for the data transformation

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -45,4 +45,7 @@ Data transformation releated constants start with the DATA_TRANSFORMATION var na
 
 """
 DATA_TRANSFORMATION_DIR_NAME : str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR : str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR : str = "transformed_object"
+
 
